adec89d90c7489edd2025680c3ac55098162b9f5
schizobulia <2833324528@qq.com>
2020-03-24 20:09:59 +0800

    add search api

--- app/controller/mirror/index.js
+++ app/controller/mirror/index.js
@@ -2,6 +2,8 @@
 
 const { Controller } = require('egg');
 const fs = require('fs');
+const FileHound = require('filehound');
+
 /**
  * 
  */
@@ -17,6 +19,29 @@ class MirrorController extends Controller {
         let list = {
             files: arr.filter((ele) => { return !ele.isDirectory() }),
             dirs: arr.filter((ele) => { return ele.isDirectory() })
+        };
+        ctx.body = { code: 1, data: list };
+    }
+
+    /**
+     * 查询
+     */
+    async search() {
+        const { ctx } = this;
+        let { dir, q } = ctx.request.body;
+        let basePath = this.config.baseDir + '/app/public/mirror' + dir
+        let files = await FileHound.create()
+            .paths(basePath)
+            .match(`*${q}*`)
+            .findSync();
+        let dirs = await FileHound.create()
+            .paths(basePath)
+            .directory()
+            .match(`*${q}*`)
+            .findSync();
+        let list = {
+            files: files.map((e) => { return { name: e.slice(basePath.length, e.length) } }),
+            dirs: dirs.map((e) => { return { name: e.slice(basePath.length, e.length) } })
         };
         ctx.body = { code: 1, data: list };
     }
--- app/router/mirror.js
+++ app/router/mirror.js
@@ -4,4 +4,5 @@
 module.exports = app => {
     const { router, controller } = app;
     router.post('/api/v1/mirror/index', controller.mirror.index.index);
+    router.post('/api/v1/mirror/search', controller.mirror.index.search);
 };
